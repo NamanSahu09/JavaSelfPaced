b68bba52526c885af98e63fe2e9f7b77d276543a
Naman Swastik Sahu <namanswastik11@gmail.com>
2024-06-03 14:39:21 +0530

    STRIVER Pattern Practice 1-6 done

---
+++ PatternsPractice/one.java
@@ -0,0 +1,17 @@
+public class one 
+{
+  public static void main(String[] args) 
+  {
+      System.out.println("Pattern Building \n");
+    for(int i=1;i<=5;i++)
+    {
+      for(int j=1;j<=5;j++)
+      {
+        System.out.print("*");
+      }     
+      System.out.println();
+    }
+    
+  }
+  
+}
---
+++ PatternsPractice/six.java
@@ -0,0 +1,18 @@
+public class six
+{
+    public static void main(String args[])
+    {
+        
+        for(int i=1;i<=5;i++)
+        {
+            int k=1;
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print(k);
+                k++;
+            }
+            System.out.println();
+        }
+        
+    }
+}
