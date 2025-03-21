762fd290e1af81661455a851680aa0ade0bd459d
Naman Swastik Sahu <namanswastik11@gmail.com>
2025-03-21 17:59:25 +0530

    sieve of erathosthenes

---
+++ StriverA2Z/Bit Manipulation/Intermediate/.history/primeNo_20250321175723.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class primeNo {
+  
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    //Sive of erathosthenes
+
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+    
+    boolean prime[] = new boolean[n+1];
+
+
+
+  }
+
+}
---
+++ StriverA2Z/Bit Manipulation/Intermediate/.history/primeNo_20250321175831.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class primeNo {
+  
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    //Sive of erathosthenes
+
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+    
+    boolean prime[] = new boolean[n+1];
+    for(int i=0;i<n;i++)
+    {
+      prime[i] = true;
+    }
+    for(int i=2;i<Math.sqrt(n);i++)
+    {
+      if(prime[i] == true)
+      {
+        for(int j = )
+      }
+    }
+
+
+  }
+
+}
